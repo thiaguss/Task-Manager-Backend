6359f573e8369169994b08f9c41efdf37642ddfe
Thiago Santos <tsantos7098@gmail.com>
2025-09-10 22:11:35 -0300

    "refactor: refactoring the connection with mongoose"

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
-const conectToDatabase = async () => {
+const connectToDatabase = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@taskmanagercluster.99kj33e.mongodb.net/?retryWrites=true&w=majority&appName=TaskManagerCluster`
-    );
-    console.log(" Connected to MongoDB");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected successfully");
   } catch (err) {
-    console.error("Error connecting to MongoDB:", err.message);
+    console.error("Database connection failed:", err.message);
     process.exit(1);
   }
 };
 
-export default conectToDatabase;
+export default connectToDatabase;
