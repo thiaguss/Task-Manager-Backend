dd862689ee2b80078d8a5cb3a1113b061455e560
Thiago Santos <tsantos7098@gmail.com>
2025-09-15 19:25:25 -0300

    "refactor: refactoring the controller into a class"

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,71 +1,79 @@
 import mongoose from "mongoose";
 import * as taskService from "../services/task.service.js";
 
-export const getTasks = async (req, res, next) => {
-  try {
-    const tasks = await taskService.getAllTasks();
-    res.status(200).json(tasks);
-  } catch (err) {
-    next(err);
+class TaskController {
+  constructor(service) {
+    this.service = service;
   }
-};
 
-export const getTaskById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    if (!mongoose.isValidObjectId(id)) {
-      const error = new Error("Invalid task ID");
-      error.statusCode = 400;
-      throw error;
+  getTasks = async (req, res, next) => {
+    try {
+      const tasks = await this.service.getAllTasks();
+      res.status(200).json(tasks);
+    } catch (err) {
+      next(err);
     }
+  };
 
-    const task = await taskService.getTaskById(id);
-    res.status(200).json(task);
-  } catch (err) {
-    next(err);
-  }
-};
+  getTaskById = async (req, res, next) => {
+    try {
+      const { id } = req.params;
 
-export const createTask = async (req, res, next) => {
-  try {
-    const newTask = await taskService.createTask(req.body);
-    res.status(201).json(newTask);
-  } catch (err) {
-    next(err);
-  }
-};
+      if (!mongoose.isValidObjectId(id)) {
+        const error = new Error("Invalid task ID");
+        error.statusCode = 400;
+        throw error;
+      }
 
-export const updateTask = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    if (!mongoose.isValidObjectId(id)) {
-      const error = new Error("Invalid task ID");
-      error.statusCode = 400;
-      throw error;
+      const task = await this.service.getTaskById(id);
+      res.status(200).json(task);
+    } catch (err) {
+      next(err);
     }
+  };
 
-    const updatedTask = await taskService.updateTask(id, req.body);
-    res.status(200).json(updatedTask);
-  } catch (err) {
-    next(err);
-  }
-};
-
-export const deleteTask = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    if (!mongoose.isValidObjectId(id)) {
-      const error = new Error("Invalid task ID");
-      error.statusCode = 400;
-      throw error;
+  createTask = async (req, res, next) => {
+    try {
+      const newTask = await this.service.createTask(req.body);
+      res.status(201).json(newTask);
+    } catch (err) {
+      next(err);
     }
+  };
 
-    await taskService.deleteTask(id);
-    res.status(200).json({ message: "Task deleted successfully." });
-  } catch (err) {
-    next(err);
-  }
-};
+  updateTask = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      if (!mongoose.isValidObjectId(id)) {
+        const error = new Error("Invalid task ID");
+        error.statusCode = 400;
+        throw error;
+      }
+
+      const updatedTask = await this.service.updateTask(id, req.body);
+      res.status(200).json(updatedTask);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  deleteTask = async (req, res, next) => {
+    try {
+      const { id } = req.params;
+
+      if (!mongoose.isValidObjectId(id)) {
+        const error = new Error("Invalid task ID");
+        error.statusCode = 400;
+        throw error;
+      }
+
+      await this.service.deleteTask(id);
+      res.status(200).json({ message: "Task deleted successfully." });
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+export default new TaskController(taskService);
--- src/routes/task.routers.js
+++ src/routes/task.routers.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { getTasks, createTask, deletedTask, getTaskById, updatedTask } from "../controllers/task.controller.js";
+import taskController from "../controllers/task.controller.js";
 
 const router = Router();
 
-router.get("/", getTasks);
-router.get("/:id", getTaskById)
-router.post("/", createTask);
-router.patch("/:id", updatedTask);
-router.post("/:id", deletedTask);
-
+router.get("/", taskController.getTasks);
+router.get("/:id", taskController.getTaskById);
+router.post("/", taskController.createTask);
+router.patch("/:id", taskController.updateTask);
+router.delete("/:id", taskController.deleteTask);
 
 export default router;
